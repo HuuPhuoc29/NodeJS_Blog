573b1a3689153809d6c991c16f1c41f232be3ac1
HuuPhuocHPH <luuvanhuuphuoc@gmail.com>
2023-02-19 00:00:39 +0700

    First commit - MVC model

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const newsRoutes = require('./news')
+const siteRoutes = require('./site')
+
+function route(app){
+  
+  app.use('/news', newsRoutes)
+ 
+  app.use('/', siteRoutes)
+}
+
+module.exports = route;
